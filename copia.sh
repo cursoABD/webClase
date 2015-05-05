2babe98131374ef7081d67cef11074515dfd51ba
ludverk <luis.seller@gmail.com>
2015-05-05 14:17:08 +0200

    Cambia fichero copia.sh

--- copia.sh
+++ copia.sh
@@ -4,6 +4,6 @@ if [ $# -lt 1 ]
 then
     echo "Introduzca el fichero a copiar"
 else
-    echo "Copiando archivo: $1 a /var/www/primero"
-    sudo cp $1 /var/www/primero
+    echo "Copiando archivo: $1 a /var/www/html/primero/"
+    sudo cp $1 /var/www/primero/
 fi
