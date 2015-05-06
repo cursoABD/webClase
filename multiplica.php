9ba0d20955a732a2dd14f9d73c6fd555c74c4fa0
CursoABD <ceveras@gmail.com>
2015-05-06 13:34:54 +0200

    Sube multiplica.php

---
+++ multiplica.php
@@ -0,0 +1,23 @@
+<!--Lo que pongo no se tiene en cuenta -->
+
+<html>
+
+    <body>
+
+    <?php
+    
+        //Esto es un comentario
+        /*Esto es otro comentario*/
+        # Esto es otro comentario más
+        
+        echo "<h2>Tabla de multiplicar</h2>";
+        
+        for($i=1;$i<11;$i++){
+            echo "7 x $i = ".(7*$i)."<br>\n";
+        }
+    ?>
+
+    </body>
+
+</html>
+
